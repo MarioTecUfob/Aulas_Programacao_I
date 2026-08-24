6e2ef4dc21543bc77cd05a4c318e5b2e919cf8cd
MarioTecUfob <tecsolucoes0101@gmail.com>
2026-08-24 11:03:22 -0300

    Mandando arquivo

---
+++ matrizes.py
@@ -0,0 +1,18 @@
+#Matriz é uma lista de lista
+numeros = [
+    [3,4,7,8],
+    [1,3,6,2],
+    [6,9,7,9],
+    [7,5,0,1]
+]
+#pega a linha e a coluna
+print(numeros[1][2])
+# Usuário escolhe uma linha
+l = int(input("Qual linha você quer ver: "))
+# Pega a linha inteira
+linha = numeros [l- 1]
+print(linha)
+print(linha[2]) #Entra com 2, lá ele'''
+print(numeros[l-1][2]) #O resultado é o mesmo, 6
+print(numeros[1][2]) #O resultado é o mesmo, 6
+print(numeros[1][2]) #O resultado é o mesmo, 6
